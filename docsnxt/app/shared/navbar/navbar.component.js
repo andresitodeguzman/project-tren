5c6c00f8614adb61fdb94ce9a2737cb1f2a3a7b5
Andresito de Guzman <andresitomyemail@gmail.com>
2019-02-11 03:38:45 +0000

    Edits

--- docsnxt/app/shared/navbar/navbar.component.js
+++ docsnxt/app/shared/navbar/navbar.component.js
@@ -1,6 +1,11 @@
 angular.module('navbar').component('navbar',{
     templateUrl:'app/shared/navbar/navbar.template.html',
     controller: function navbarController(){
+        this.color = "transparent";
+        this.shadow = "z-depth-0";
+        this.title = "Hello PH";
+        this.titleColor = "white-text";
+
       // Some controller functions here
     }
 });
