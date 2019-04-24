d657ba14b3ac2b5b95b91ceae853354f6172c373
Andresito de Guzman <andresitomyemail@gmail.com>
2019-04-24 23:59:22 +0800

    added lrt twitter handle

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let cn = '1.03';
-let cacheWhiteList = ['1.03'];
+let cn = '1.04';
+let cacheWhiteList = ['1.04'];
 let assetsList = [
     '/project-tren/index.html',
     '/project-tren/index.js',
