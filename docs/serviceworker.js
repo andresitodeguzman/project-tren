d86e132a0d84f59ecfaaadaac89092e23454425e
Andresito de Guzman <andresitomyemail@gmail.com>
2019-05-13 21:42:19 +0800

    hotfix

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let cn = '1.07';
-let cacheWhiteList = ['1.07'];
+let cn = '1.08';
+let cacheWhiteList = ['1.08'];
 let assetsList = [
     '/project-tren/index.html',
     '/project-tren/index.js',
